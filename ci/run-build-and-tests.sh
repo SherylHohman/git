2deb3ac117768133551716c9788266671aafb445
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-07-26 13:18:26 +0200

    fixup??? ci: run trace2 schema validation in the CI suite
    That seems like an oversight when iterating the patch series.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ci/run-build-and-tests.sh
+++ ci/run-build-and-tests.sh
@@ -24,8 +24,8 @@ linux-gcc)
 	export GIT_TEST_ADD_I_USE_BUILTIN=1
 	make test
 	t/trace_schema_validator/trace_schema_validator \
-		--trace2_event_file=${GIT_TRACE2_EVENT} \
-		--schema_file=t/trace_schema_validator/strict_schema.json \
+		--trace2-event-file=${GIT_TRACE2_EVENT} \
+		--schema-file=t/trace_schema_validator/strict_schema.json \
 		--progress=10000
 	;;
 linux-gcc-4.8)
